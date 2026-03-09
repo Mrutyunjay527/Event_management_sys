014da08aad133a95dae8e2bc204557a5048324ce
mjay1 <mjay1@LAPTOP-DRB68GH9>
2026-03-09 10:49:44 +0530

    Add project files.

---
+++ Event_management_sys/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using Event_management_sys.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Event_management_sys.Controllers
+{
+    public class EventsController : Controller
+    {
+        private static List<Event> events = new List<Event>();
+        public IActionResult Index()
+        {
+            return View(events);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Event ev)
+        {
+            events.Add(ev);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Event_management_sys/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Event_management_sys.Models
+{
+    public class Event
+    {
+        public int ID { get; set; } 
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public DateTime EventDate { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
